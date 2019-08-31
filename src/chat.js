55faa9af394d8ae42c6df489060c47dc32cb8fcf
belenot <kholhunov0@gmail.com>
2019-08-31 15:49:43 +0300

    websocket was added

--- src/chat.js
+++ src/chat.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs'
 import {api} from './api/api';
 import {StyledChatPane} from './components/styled/StyledChatPane';
 
 window.React = React;
 window.api = api;
+var login = 'log';
+var session;
 
-var sendMessage = (message, callback) => {
-    api.sendMessage(JSON.stringify(message), r => callback(JSON.parse(r)));
+var sendMessage = (message) => {
+    //api.sendMessage(JSON.stringify(message), r => callback(JSON.parse(r)));
+    client.send("/app/message", {session}, JSON.stringify(message));
+}
+var getMessages;
+var provideMessages = (callback) => {
+    getMessages = callback;
 }
 
+var recieveMessage= (r) =>{
+    rerender([...getMessages(), JSON.parse(r.body)]);
+}
 
 var rerender = (messages) => {
     ReactDOM.render(
-        <StyledChatPane initMessages={messages} sendMessage={sendMessage}/>,
+        <StyledChatPane initMessages={messages} sendMessage={sendMessage} provideMessages={provideMessages}/>,
         document.querySelector("#chat-pane")
     )
 }
 
+var ws = new SockJS("http://localhost:8080/chat/ws");
+var client = Stomp.over(ws);
+window.ws = ws;
+window.client = client;
+client.debug(console.log);
+var connected = false;
+var subscription;
+client.connect({}, () => {
+    subscription = client.subscribe("/topic/message", recieveMessage);
+    session = client.ws._transport.url.split('/').slice(-2, -1)
+});
+
+
+
 window.onload = () => {
     api.getMessages(r => rerender(JSON.parse(r)));
+}
+window.onclose= () => {
+    subscription.unsubscribe();
+    client.disconnect();
 }
--- src/components/ChatPane.js
+++ src/components/ChatPane.js
@@ -2,14 +2,15 @@ import {StyledMessage} from './styled/StyledMessage';
 import {MessageForm} from './MessageForm'
 import {useState, useEffect} from 'react'
 
-export const ChatPane = ({className, initMessages=[], sendMessage=f=>f}) => {
-    let [messages, setMessages] = useState(initMessages);
-    let callback = (newMessages) => {
-        setMessages([...messages, newMessages]);
+export const ChatPane = ({className, initMessages=[], sendMessage=f=>f, provideMessages=f=>f}) => {
+    let [messages, setMessages] = useState(initMessages);    
+    if (initMessages.length > messages.length) {
+        setMessages([...initMessages]);
     }
+    provideMessages(() => messages);
     let onSendMessage = (text) => {
         let message = {text};
-        sendMessage(message, callback);
+        sendMessage(message);
     }
 
     return (
